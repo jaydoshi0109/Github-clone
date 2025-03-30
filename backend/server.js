9bf521df495c818201ddc490b1047d98f7904a21
jaydoshi0109 <jaydoshi0109@gmail.com>
2025-03-31 01:35:16 +0530

    fixed server.js

--- backend/server.js
+++ backend/server.js
@@ -46,12 +46,6 @@ app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/explore", exploreRoutes);
 
-app.use(express.static(path.join(__dirname, "/frontend/dist")));
-
-app.get("*", (req, res) => {
-	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
-});
-
 app.listen(PORT, () => {
 	console.log(`Server started on http://localhost:${PORT}`);
 	connectMongoDB();
