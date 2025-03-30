fd2074bb33b0c8b14438696b6e898dfe45aed1c0
jaydoshi0109 <jaydoshi0109@gmail.com>
2025-03-31 01:17:27 +0530

    auth.route.js

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -6,11 +6,18 @@ const router = express.Router();
 router.get("/github", passport.authenticate("github", { scope: ["user:email"] }));
 
 router.get(
-	"/github/callback",
-	passport.authenticate("github", { failureRedirect: process.env.CLIENT_BASE_URL + "/login" }),
-	function (req, res) {
-		res.redirect(process.env.CLIENT_BASE_URL);
-	}
+    "/github/callback",
+    passport.authenticate("github", { failureRedirect: process.env.CLIENT_BASE_URL + "/login" }),
+    function (req, res) {
+        req.login(req.user, (err) => {
+            if (err) {
+                console.error("Error during req.login:", err);
+                return next(err);
+            }
+            console.log("User logged in via req.login:", req.user);
+            res.redirect(process.env.CLIENT_BASE_URL);
+        });
+    }
 );
 
 router.get("/check", (req, res) => {
