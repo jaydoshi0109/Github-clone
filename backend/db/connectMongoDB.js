0a4538b598deedf4597b8410af43177ede74cb38
jaydoshi0109 <jaydoshi0109@gmail.com>
2025-03-31 03:18:18 +0530

    fixed

--- backend/db/connectMongoDB.js
+++ backend/db/connectMongoDB.js
@@ -1,6 +1,13 @@
-await mongoose.connect(process.env.MONGO_URI, {
-	useNewUrlParser: true,
-	useUnifiedTopology: true,
-  });
+import mongoose from "mongoose";
 
-console.log("MongoDB Connected!");  
+export default async function connectMongoDB() {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("MONGODB connected");
+  } catch (error) {
+    console.log("Error connecting to MongoDB: ", error.message);
+  }
+}
