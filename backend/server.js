31573982f0ac2677c73a9050bd5c9ee196011835
jaydoshi0109 <jaydoshi0109@gmail.com>
2025-03-31 11:41:57 +0530

    fixed AUTHROUTE AND SEREVR.JS

--- backend/server.js
+++ backend/server.js
@@ -70,7 +70,7 @@ app.use(passport.session());
 
 // Optional: Debug middleware to log session and user info for each request
 app.use((req, res, next) => {
-  console.log(`Session ID: ${req.sessionID} | User: req.user}`);
+  console.log(`Session ID: ${req.sessionID} | User: ${req.user}`);;
   next();
 });
 
